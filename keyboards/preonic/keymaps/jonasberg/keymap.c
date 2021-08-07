a815f7548c8db126b0c94b1ee954f13f6aad4b73
Jonas Schoenenberger <jonas.schoenenberger@pm.me>
2021-08-07 10:09:00 +0200

    switch volume up and volume down

--- keyboards/preonic/keymaps/jonasberg/keymap.c
+++ keyboards/preonic/keymaps/jonasberg/keymap.c
@@ -162,7 +162,7 @@ TD(TD_LSFT), KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COM
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |             |      | Next | Vol+ | Vol- | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid(
@@ -170,7 +170,7 @@ TD(TD_LSFT), KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COM
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_LBRC,   KC_RBRC,    KC_DEL,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  S(KC_LBRC),S(KC_RBRC), KC_BSLS,
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLU , KC_VOLD , KC_MPLY
 ),
 
 /* Adjust (Lower + Raise)
