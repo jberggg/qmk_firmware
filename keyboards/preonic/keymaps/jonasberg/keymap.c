7688c7991cb7c6e3cc835cf73b3bf6eaa13f5378
Jonas Schoenenberger <jonas.schoenenberger@pm.me>
2022-05-08 14:15:48 +0200

    add umlaut-free qwertz layout

--- keyboards/preonic/keymaps/jonasberg/keymap.c
+++ keyboards/preonic/keymaps/jonasberg/keymap.c
@@ -18,6 +18,7 @@
 #include "muse.h"
 
 enum preonic_layers {
+  _QWERTZ_UMLAUTE,
   _QWERTZ,
   _COLEMAK,
   _LOWER,
@@ -26,7 +27,8 @@ enum preonic_layers {
 };
 
 enum preonic_keycodes {
-  QWERTZ = SAFE_RANGE,
+  QWERTZ_UMLAUTE = SAFE_RANGE,
+  QWERTZ,
   COLEMAK,
   LOWER,
   RAISE,
@@ -93,6 +95,27 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+/* Qwertz w. double tab Umlaute
+ * ,-----------------------------------------------------------------------------------.
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | `    |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Del  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Up   | Down |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_QWERTZ_UMLAUTE] = LAYOUT_preonic_grid(
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Z, TD(TD_UÜ),  KC_I,  TD(TD_OÖ), KC_P,    KC_DEL,
+  KC_TAB, TD(TD_AÄ), KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+ TD(TD_LSFT), KC_Y , KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,TD(TD_SLSH),KC_ENT,
+  RGB_TOG, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   TD(TD_LEFT), KC_UP,   KC_DOWN,  TD(TD_RIGHT)
+),
+
 /* Qwertz
  * ,-----------------------------------------------------------------------------------.
  * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
@@ -108,9 +131,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTZ] = LAYOUT_preonic_grid(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Z, TD(TD_UÜ),  KC_I,  TD(TD_OÖ), KC_P,    KC_DEL,
-  KC_TAB, TD(TD_AÄ), KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
- TD(TD_LSFT), KC_Y , KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,TD(TD_SLSH),KC_ENT,
+  KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Z,     KC_U,  KC_I,      KC_O, KC_P,    KC_DEL,
+  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  TD(TD_LSFT), KC_Y , KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,TD(TD_SLSH),KC_ENT,
   RGB_TOG, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   TD(TD_LEFT), KC_UP,   KC_DOWN,  TD(TD_RIGHT)
 ),
 
@@ -181,20 +204,20 @@ TD(TD_LSFT), KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COM
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Reset| Debug|      |      |      |      |TermOf|TermOn| OSX  | Qwrz |  Del |
+ * |      | Reset| Debug|      |      |      |      |TermOf|TermOn| OSX  | Umla |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |Aud cy|Aud on|AudOff|AGnorm|AGswap|      |      | LNX  | Clmk |      |
+ * |      |      |Aud cy|Aud on|AudOff|AGnorm|AGswap|      |      | LNX  | Qwrz |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      |      |      |      |
+ * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      |      | Clmk |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_grid(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF, OSX_MODE, QWERTZ, KC_DEL,
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, LINUX_MODE, COLEMAK, _______,
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
+  _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF, OSX_MODE, QWERTZ_UMLAUTE, KC_DEL,
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, LINUX_MODE, QWERTZ, _______,
+  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, COLEMAK, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
@@ -202,6 +225,12 @@ TD(TD_LSFT), KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COM
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+        case QWERTZ_UMLAUTE:
+          if (record->event.pressed) {
+            set_single_persistent_default_layer(_QWERTZ_UMLAUTE);
+          }
+          return false;
+          break;
         case QWERTZ:
           if (record->event.pressed) {
             set_single_persistent_default_layer(_QWERTZ);
